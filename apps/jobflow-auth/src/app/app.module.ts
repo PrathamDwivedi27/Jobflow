0ec6ab2a09e65093bfd217b2d2b0cd6fc23cfcca
Pratham-fursatAI <prathamd549@gmail.com>
2026-06-22 22:32:12 +0530

    feat: make config module

--- apps/jobflow-auth/src/app/app.module.ts
+++ apps/jobflow-auth/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import {GraphQLModule} from '@nestjs/graphql'
+import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PrismaModule,
     UsersModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       graphiql: true,
-      autoSchemaFile: true, 
-    })
+      autoSchemaFile: true,
+    }),
   ],
   controllers: [],
   providers: [],
