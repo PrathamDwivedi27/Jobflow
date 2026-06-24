f9d289d5eee2825c2941d14e33a899ba0f886dc3
Pratham-fursatAI <prathamd549@gmail.com>
2026-06-24 13:32:53 +0530

    feat: implemented job resolver

--- apps/jobflow-jobs/src/app/app.module.ts
+++ apps/jobflow-jobs/src/app/app.module.ts
@@ -1,9 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), JobsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- apps/jobflow-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobflow-jobs/src/app/jobs/jobs.module.ts
@@ -2,9 +2,10 @@ import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 import { Module } from '@nestjs/common';
 import { FibonacciJob } from './fibonacci/fibonacci.job';
 import { JobsService } from './jobs.service';
+import { JobsResolver } from './jobs.resolver';
 
 @Module({
   imports: [DiscoveryModule],
-  providers: [FibonacciJob, JobsService],
+  providers: [FibonacciJob, JobsService, JobsResolver],
 })
 export class JobsModule {}
