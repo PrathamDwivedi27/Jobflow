115554c610f67ee7d0d1b0197b06f96ca3e84501
Pratham-fursatAI <prathamd549@gmail.com>
2026-06-24 12:37:04 +0530

    feat: job finder method is done

---
+++ apps/jobflow-jobs/src/app/jobs/jobs.module.ts
@@ -0,0 +1,10 @@
+import { DiscoveryModule } from '@golevelup/nestjs-discovery';
+import { Module } from '@nestjs/common';
+import { FibonacciJob } from './fibonacci/fibonacci.job';
+import { JobsService } from './jobs.service';
+
+@Module({
+  imports: [DiscoveryModule],
+  providers: [FibonacciJob, JobsService],
+})
+export class JobsModule {}
