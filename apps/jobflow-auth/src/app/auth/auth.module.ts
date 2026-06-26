e6f96851cd0155bb20e57ecf567d0789640b1de5
Pratham-fursatAI <prathamd549@gmail.com>
2026-06-26 12:10:00 +0530

    feat: client side auth proto is setup

--- apps/jobflow-auth/src/app/auth/auth.module.ts
+++ apps/jobflow-auth/src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     }),
     UsersModule,
   ],
+  controllers: [AuthController],
   providers: [AuthResolver, AuthService, JwtStrategy],
 })
 export class AuthModule {}
