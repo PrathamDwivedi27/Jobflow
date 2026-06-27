fae475c246a106b5eb1454e595be470e8979aa6f
Pratham-fursatAI <prathamd549@gmail.com>
2026-06-27 08:54:42 +0530

    feat: pulsar client is setup

---
+++ libs/pulsar/src/lib/pulsar.client.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { Client } from 'pulsar-client';
+
+@Injectable()
+export class PulsarClient implements OnModuleDestroy {
+  private readonly client = new Client({
+    serviceUrl: this.configService.getOrThrow<string>('PULSAR_SERVICE_URL'),
+  });
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async createProducer(topic: string) {
+    return this.client.createProducer({
+      topic,
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.client.close();
+  }
+}
